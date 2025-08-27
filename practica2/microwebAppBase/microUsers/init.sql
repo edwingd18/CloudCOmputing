6024bee3ff99b4032b7dc67a94f7b96a25fd25bf
edwingd18 <edwin.guerrero@uao.edu.co>
2025-08-27 02:35:34 -0500

    ?

--- practica2/microwebAppBase/microUsers/init.sql
+++ practica2/microwebAppBase/microUsers/init.sql
@@ -1,5 +1,4 @@
-use myflaskapp;
-
+use myflaskapp_users;
 CREATE TABLE users (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(255),
@@ -7,14 +6,6 @@ CREATE TABLE users (
     username varchar(255),
     password varchar(255)
 );
-CREATE TABLE products (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255),
-    price DECIMAL(10,2),
-    stock int,
-    description varchar(255)
-);
-
 INSERT INTO users VALUES(null, "juan", "juan@gmail.com", "juan", "123"),
     (null, "maria", "maria@gmail.com", "maria", "456");
 
--- practica2/microwebAppBase/microUsers/users/views.py
+++ practica2/microwebAppBase/microUsers/users/views.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
 
