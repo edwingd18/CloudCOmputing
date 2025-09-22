d93ed25341db91a9c1d203a62b393acef6490f00
edwingd18 <edwin.guerrero@uao.edu.co>
2025-09-22 13:28:57 -0500

    Aprovisionamiento de practica7

---
+++ practica7/provision_k8s.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script de aprovisionamiento para Minikube y Kubectl
+
+echo "=== Iniciando aprovisionamiento de Minikube ==="
+
+# Actualizar paquetes
+sudo apt-get update -y
+
+# Instalar minikube
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+#Instale kubectl, una interfaz de línea de comandos para ejecutar comandos en clusters Kubernetes.
+
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+
+
